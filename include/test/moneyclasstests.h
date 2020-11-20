939d54c3566d5a8f51eb709d573c448d0bed8f39
EthanBayer <bayeres1871@gmail.com>
2020-11-19 22:31:46 -0800

    Added the test file for the money class.

--- include/test/moneyclasstests.h
+++ include/test/moneyclasstests.h
@@ -13,10 +13,40 @@ TEST(MoneyclassTest, GetDollarTest)
     EXPECT_EQ(test->getDollar(), 123);
 
     MoneyClass* test2 = new MoneyClass(987.65);
-    EXPECT_EQ(test->getDollar(), 987);
+    EXPECT_EQ(test2->getDollar(), 987);
 
-    test->setMoney(1.78);
-    EXPECT_EQ(test->getDollar(), 1);
+    test2->setMoney(1.78);
+    EXPECT_EQ(test2->getDollar(), 1);
+}
+
+TEST(MoneyclassTest, GetCentTest)
+{
+    MoneyClass* test = new MoneyClass();
+    EXPECT_EQ(test->getCent(), 00);
+
+    test->setMoney(123.45);
+    EXPECT_EQ(test->getCent(), 45);
+
+    MoneyClass* test2 = new MoneyClass(987.65);
+    EXPECT_EQ(test2->getCent(), 65);
+
+    test2->setMoney(1.78);
+    EXPECT_EQ(test2->getCent(), 78);
+}
+
+TEST(MoneyclassTest, GetTotalTest)
+{
+    MoneyClass* test = new MoneyClass();
+    EXPECT_EQ(test->getTotal(), "0.0");
+
+    test->setMoney(123.45);
+    EXPECT_EQ(test->getTotal(), "123.45");
+
+    MoneyClass* test2 = new MoneyClass(987.65);
+    EXPECT_EQ(test2->getTotal(), "987.65");
+
+    test2->setMoney(1.78);
+    EXPECT_EQ(test2->getTotal(), "1.78");
 }
 
 #endif
