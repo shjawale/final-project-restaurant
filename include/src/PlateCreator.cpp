f5dc628ac4e57ed560f78343270ae5b105c5c8a4
shjawale <shruti.s.jawale@gmail.com>
2020-11-23 10:02:20 -0800

    modified PlateCreator.cpp

--- include/src/PlateCreator.cpp
+++ include/src/PlateCreator.cpp
@@ -2,7 +2,7 @@
 #include "../header/moneyclass.h"
 #include <string>
 
-Item* PlateCreator::CreateItem(std::string* name, MoneyClass* price, std::vector*<std::string> modification){
+Item* PlateCreator::CreateItem(std::string name, MoneyClass price, std::vector&<std::string> modification){
 	Item* item = new Plate();
 	item->setName(name);
 	item->setPrice(price);
