05fdecf20995ba2d74e219467b5533b6d4902b51
shjawale <shruti.s.jawale@gmail.com>
2020-11-23 10:02:04 -0800

    modified SingleItemCreator.cpp

--- include/src/SingleItemCreator.cpp
+++ include/src/SingleItemCreator.cpp
@@ -2,7 +2,7 @@
 #include "../header/moneyclass.h"
 #include <string>
 
-Item* SingleItemCreator::CreateItem(std::string* name, MoneyClass* price, std::vector*<std::string> modification){
+Item* SingleItemCreator::CreateItem(std::string name, MoneyClass price, std::vector&<std::string> modification){
 	Item* item = new SingleItem();
 	item->setName(name);
 	item->setPrice(price);
