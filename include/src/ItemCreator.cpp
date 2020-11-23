1d0aa9a74683399ffaf048be5a2e8235882aeb6b
shjawale <shruti.s.jawale@gmail.com>
2020-11-22 23:49:11 -0800

    Added ItemCreator

---
+++ include/src/ItemCreator.cpp
@@ -0,0 +1,15 @@
+#include "../header/Item.h"
+#include "../header/moneyclass.h"
+#include <string>
+
+Item* ItemCreator::CreateItem(std::string* name, MoneyClass* price, std::vector<std::string> modification){
+	MoneyClass money = new MoneyClass(price);
+    money->setPrice(price);
+
+	Item* item = new Item();
+	item->setName(name);
+	//item->setPrice(price);
+	//item->setModifications(modification);
+	return item;
+}
+
