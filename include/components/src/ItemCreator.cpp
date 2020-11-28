39795a6181d29a02179fc0b76083ea4a22ed6128
shjawale <shruti.s.jawale@gmail.com>
2020-11-28 00:32:39 -0800

    modified ItemCreator

--- include/components/src/ItemCreator.cpp
+++ include/components/src/ItemCreator.cpp
@@ -1,11 +1,13 @@
 #include "../headers/ItemCreator.hpp"
 #include "../headers/Item.h"
+#include "../headers/plate.h"
+#include "../headers/SingleItem.hpp"
 #include "../headers/moneyclass.h"
 #include <string>
 
 Item* ItemCreator::CreateItem(ItemType TypeId, std::string name, MoneyClass price, std::vector<std::string> modification){
 	if(TypeId == PLATE_ITEM_T){
-		Item* item = new Plate();
+		Plate* item = new Plate;
 		item->setName(name);
 		item->setPrice(price);
 		item->setModifications(modification);
@@ -13,7 +15,7 @@ Item* ItemCreator::CreateItem(ItemType TypeId, std::string name, MoneyClass pric
 	}
 
 	if(TypeId == SINGLE_ITEM_T){
-	    Item* item = new SingleItem();
+		SingleItem* item = new SingleItem;
         item->setName(name);
         item->setPrice(price);
         item->setModifications(modification);
