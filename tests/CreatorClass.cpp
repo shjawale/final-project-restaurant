e893dcce8e171e6f89f7d5aa4477dbe49196f147
shjawale <shruti.s.jawale@gmail.com>
2020-12-04 22:54:49 -0800

    Modified Item, SingleItem, ItemCreator

---
+++ tests/CreatorClass.cpp
@@ -0,0 +1,61 @@
+#include "gtest/gtest.h"
+
+#include "../include/components/headers/Item.h"
+#include "../include/components/headers/moneyclass.h"
+#include "../include/components/headers/ItemCreator.hpp"
+#include "../include/components/headers/plate.h"
+#include "../include/components/headers/SingleItem.hpp"
+
+TEST(ItemCreatorTest, GetSingleItemTest){
+    std::string name = "water";
+
+    MoneyClass* money = new MoneyClass();
+    money->setMoney(0.75);
+
+    ItemCreator itemCreator;
+    Item* item = itemCreator.CreateItem(SINGLE_ITEM_T, name, *money);
+    EXPECT_EQ(item->getName(), "water");
+    EXPECT_EQ(item->getPrice().getTotal(), "0.75");
+}
+
+TEST(ItemCreatorTest, GetPlateTest){
+    std::string name = "burrito";
+
+    MoneyClass* money = new MoneyClass();
+    money->setMoney(12.45);
+
+    ItemCreator itemCreator;
+    Item* item = itemCreator.CreateItem(PLATE_ITEM_T, name, *money);
+    EXPECT_EQ(item->getName(), "burrito");
+    EXPECT_EQ(item->getPrice().getTotal(), "12.45");
+}
+
+
+TEST(ItemCreatorTest, GetSingleItemTest2){
+    std::string name = "orange juice";
+
+    MoneyClass* money = new MoneyClass();
+    money->setMoney(1.75);
+
+    ItemCreator itemCreator;
+    Item* item = itemCreator.CreateItem(SINGLE_ITEM_T, name, *money);
+    EXPECT_EQ(item->getName(), "orange juice");
+    EXPECT_EQ(item->getPrice().getTotal(), "1.75");
+}
+
+TEST(ItemCreatorTest, GetPlateTest2){
+    std::string name = "burger";
+
+    MoneyClass* money = new MoneyClass();
+    money->setMoney(12.95);
+
+	ItemCreator itemCreator;
+	Item* item = itemCreator.CreateItem(PLATE_ITEM_T, name, *money);
+	EXPECT_EQ(item->getName(), "burger");
+	EXPECT_EQ(item->getPrice().getTotal(), "12.95");
+}
+
+int main(int argc, char** argv){
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
